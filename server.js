0b88b59d7a55889270b5525fb0e374b9c90bdfdf
Jottahdevjr <juniorjonathan164@gmail.com>
2026-08-17 11:06:55 -0300

    Primeiro projeto full stack

---
+++ server.js
@@ -0,0 +1,39 @@
+require('dotenv').config();
+
+const express = require('express');
+const cors = require('cors');
+const axios = require('axios');
+
+const app = express();
+
+app.use(cors()); 
+app.use(express.json());
+
+app.get('/api/movies', async (req, res) => {
+
+    const query = req.query.search;
+
+    if (!query) {
+        return res.status(400).json({ error: 'Termo de busca é obrigatório' });
+    }
+
+    try {
+        const response = await axios.get('https://api.themoviedb.org/3/search/movie', {
+            params: {
+                api_key: process.env.TMDB_API_KEY,
+                query: query,
+                language: 'pt-BR'
+            }
+    });
+
+    res.json(response.data.results);
+    }catch (error) {
+        res.status(500).json({ error: 'Erro ao buscar filmes no servidor' });
+    }
+})
+
+const PORT = process.env.PORT || 3000
+
+app.listen(PORT, () => {
+    console.log(`Servidor rodando na porta ${PORT}`);
+});
